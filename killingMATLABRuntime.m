db7ce2ac1976f7f844c886a73428ed5300f64235
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2024-08-13 10:37:56 -0400

    Functions migrated to appUtil class

--- killingMATLABRuntime.m
+++
@@ -1,8 +0,0 @@
-function killingMATLABRuntime(executionMode)
-
-    if ismember(executionMode, {'desktopStandaloneApp', 'webapp'})
-        pidMatlab = feature('getpid');
-        system(sprintf('taskkill /F /PID %d', pidMatlab));
-    end
-
-end
