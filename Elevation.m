1b0cd1ce7c5df0808d37a936a46a24b56ac4ad84
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2024-09-01 14:27:03 -0400

    Update Elevation.m

--- Elevation.m
+++ Elevation.m
@@ -3,9 +3,9 @@ classdef Elevation < handle
     properties
         %-----------------------------------------------------------------%
         cacheFolder
-        cacheMapping = table('Size',          [0,8],                                                                    ...
-                             'VariableTypes', {'cell', 'cell', 'double', 'double', 'double', 'double', 'cell', 'cell'}, ...
-                             'VariableNames', {'Server', 'Size', 'Lat1', 'Long1', 'Lat2', 'Long2', 'File', 'Timestamp'});
+        cacheMapping = table('Size',          [0, 7],                                                           ...
+                             'VariableTypes', {'cell', 'double', 'double', 'double', 'double', 'cell', 'cell'}, ...
+                             'VariableNames', {'Server', 'Lat1', 'Long1', 'Lat2', 'Long2', 'File', 'Timestamp'});
     end
 
 
@@ -31,25 +31,25 @@ classdef Elevation < handle
         end
 
         %-----------------------------------------------------------------%
-        function [zLine, msgWarning] = Get(obj, txInfo, rxInfo, nPoints, ForceSearch, Server)
+        function [wayPoints3D, msgWarning] = Get(obj, txInfo, rxInfo, nPoints, ForceSearch, Server)
             arguments
                 obj
                 txInfo      struct % struct('Latitude', {}, 'Longitude', {})
                 rxInfo      struct % struct('Latitude', {}, 'Longitude', {})
-                nPoints     double  = 256
+                nPoints     uint16  {mustBeGreaterThanOrEqual(nPoints, 64), mustBeLessThanOrEqual(nPoints, 1024)} = 256
                 ForceSearch logical = false
-                Server      char {mustBeMember(Server, {'Open-Elevation', 'Mathworks WMS Server'})} = 'Open-Elevation'                
+                Server      char {mustBeMember(Server, {'Open-Elevation', 'MathWorks WMS Server'})} = 'Open-Elevation'                
             end
 
-            wayPoints2D = WayPoints2D(txInfo, rxInfo, nPoints);
+            wayPoints2D = WayPoints2D(obj, txInfo, rxInfo, nPoints);
             if ForceSearch
-                [zLine, msgWarning] = WebRequest(obj, Server, wayPoints2D);
+                [wayPoints3D, msgWarning] = WebRequest(obj, Server, wayPoints2D);
             
             else
-                zLine = checkCache(obj, wayPoints2D);
+                wayPoints3D = checkCache(obj, wayPoints2D);
                 msgWarning = '';                
-                if isempty(zLine)
-                    [zLine, msgWarning] = WebRequest(obj, Server, wayPoints2D);
+                if isempty(wayPoints3D)
+                    [wayPoints3D, msgWarning] = WebRequest(obj, Server, wayPoints2D);
                 end
             end
         end
@@ -58,23 +58,18 @@ classdef Elevation < handle
 
     methods (Access = private)
         %-----------------------------------------------------------------%
-        function wayPoints2D = WayPoints2D(txInfo, rxInfo, nPoints)
-            wayPoints2D = gcwaypts(txInfo.Latitude, txInfo.Longitude, rxInfo.Latitude, rxInfo.Longitude, nPoints-1);
-        end
-
-        %-----------------------------------------------------------------%
-        function [zLine, msgWarning] = WebRequest(obj, Server, wayPoints2D)
-            zLine = [];
+        function [wayPoints3D, msgWarning] = WebRequest(obj, Server, wayPoints2D)
+            wayPoints3D = [];
             
             try
                 switch Server
                     case 'Open-Elevation'
                         % https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933|-12.5,-38.5
-                        APIRequest = obj.URL + strjoin(string(wayPoints2D(:,1)) + "," + string(wayPoints2D(:,2)), '|');
-                        APIAnswer  = webread(APIRequest, weboptions('Timeout', 10));
+                        APIRequest  = obj.URL + strjoin(string(wayPoints2D(:,1)) + "," + string(wayPoints2D(:,2)), '|');
+                        APIAnswer   = webread(APIRequest, weboptions('Timeout', 10));
                 
-                        zLine = cell2mat(cellfun(@(x) [x.latitude, x.longitude x.elevation], APIAnswer.results, 'UniformOutput', false));
-                        msgWarning = add2Cache(obj, Server, wayPoints2D, zLine);
+                        wayPoints3D = cell2mat(cellfun(@(x) [x.latitude, x.longitude x.elevation], APIAnswer.results, 'UniformOutput', false));
+                        msgWarning  = add2Cache(obj, Server, wayPoints2D, wayPoints3D);
                 
                     case 'MathWorks WMS Server'
                         WMSLayerObject = wmsfind('mathworks', 'SearchField', 'serverurl');
@@ -85,8 +80,8 @@ classdef Elevation < handle
                 
                         [zMatrix, zMatrixReference] = wmsread(WMSLayerObject, 'Latlim', [Lat1, Lat2], 'Lonlim', [Long1, Long2], 'ImageFormat', 'image/bil');
                         zMatrix = double(zMatrix);
-                        zLine = ZLine(obj, wayPoints2D, zMatrix, zMatrixReference);
-                        msgWarning = add2Cache(obj, Server, wayPoints2D, zMatrix, zMatrixReference);
+                        wayPoints3D = WayPoints3D(obj, wayPoints2D, zMatrix, zMatrixReference);
+                        msgWarning  = add2Cache(obj, Server, wayPoints2D, zMatrix, zMatrixReference);
                 end
 
             catch ME
@@ -97,31 +92,37 @@ classdef Elevation < handle
         %-----------------------------------------------------------------%
         function msgWarning = add2Cache(obj, Server, wayPoints2D, varargin)
             switch Server
-                case 'Open Elevation'
-                    zLine = varargin{1};
-                    Size = size(zLine);
+                case 'Open-Elevation'
+                    wayPoints3D = varargin{1};
                     [Lat1, Long1, Lat2, Long2] = Bounds(obj, wayPoints2D);
 
                 case 'MathWorks WMS Server'
                     zMatrix = varargin{1};
                     zMatrixReference = varargin{2};
-                    Size = size(zMatrix);
-                    [Lat1,  Lat2]  = zMatrixReference.LatitudeLimits;
-                    [Long1, Long2] = zMatrixReference.LongitudeLimits;
+
+                    Lat1  = zMatrixReference.LatitudeLimits(1);
+                    Lat2  = zMatrixReference.LatitudeLimits(2);
+                    Long1 = zMatrixReference.LongitudeLimits(1);
+                    Long2 = zMatrixReference.LongitudeLimits(2);
             end
 
-            fileName = fullfile(obj.cacheFolder, Server, datestr(now, 'yyyy.mm'), [char(matlab.lang.internal.uuid()) '.mat']);            
-            obj.cacheMapping(end+1, :) = {Server, strjoin(string(Size), 'x'), Lat1, Long1, Lat2, Long2, fileName, datestr(now)};
+            fileFolder = fullfile(obj.cacheFolder, Server, datestr(now, 'yyyy.mm'));
+            if ~isfolder(fileFolder)
+                mkdir(fileFolder)
+            end
+
+            fileName = fullfile(fileFolder, [char(matlab.lang.internal.uuid()) '.mat']);            
+            obj.cacheMapping(end+1, :) = {Server, Lat1, Long1, Lat2, Long2, fileName, datestr(now)};
 
             try
                 switch Server
-                    case 'Open Elevation'
-                        save(fileName, 'zLine', '-v7.3', '-nocompression')
+                    case 'Open-Elevation'
+                        save(fileName, 'wayPoints3D', '-v7.3', '-nocompression')
     
                     case 'MathWorks WMS Server'
                         save(fileName, 'zMatrix', 'zMatrixReference', '-v7.3', '-nocompression')
                 end
-                writetable(obj.cacheMapping(end, :), obj.cacheFile, 'WriteMode', 'append', 'AutoFitWidth', false);
+                writetable(obj.cacheMapping(end, :), fullfile(obj.cacheFolder, obj.cacheFile), 'WriteMode', 'append', 'AutoFitWidth', false);
                 msgWarning = '';
             catch ME
                 msgWarning = ME.message;
@@ -137,13 +138,13 @@ classdef Elevation < handle
         end
 
         %-----------------------------------------------------------------%
-        function zLine = checkCache(obj, wayPoints2D)
+        function wayPoints3D = checkCache(obj, wayPoints2D)
             [Lat1, Long1, Lat2, Long2] = Bounds(obj, wayPoints2D);
 
-            % A procura inicial é restrita às informações obtidas em 'Open Elevation'.
+            % A procura inicial é restrita às informações obtidas em 'Open-Elevation'.
             % Não identificando a informação requerida, procura-se no rol de
-            % informações obtidas em 'Mathworks WMS Server'.
-            cacheValidation1 = strcmp(obj.cacheMapping.Server, 'Open Elevation');
+            % informações obtidas em 'MathWorks WMS Server'.
+            cacheValidation1 = strcmp(obj.cacheMapping.Server, 'Open-Elevation');
             cacheValidation2 = abs(obj.cacheMapping.Lat1  - Lat1)  <= obj.floatTol & ...
                                abs(obj.cacheMapping.Lat2  - Lat2)  <= obj.floatTol & ...
                                abs(obj.cacheMapping.Long1 - Long1) <= obj.floatTol & ...
@@ -158,32 +159,38 @@ classdef Elevation < handle
                 idxCache = find(~cacheValidation1 & cacheValidation3, 1);
             end
 
-            zLine = [];
+            wayPoints3D = [];
             if ~isempty(idxCache)
                 fileName = obj.cacheMapping.File{idxCache};
 
                 switch obj.cacheMapping.Server{idxCache}
-                    case 'Open Elevation'
-                        load(fileName, 'zLine')
+                    case 'Open-Elevation'
+                        load(fileName, 'wayPoints3D')
                         
-                        nCachePoints = numel(zLine);
                         nWayPoints   = height(wayPoints2D);
-                        if nCachePoints ~= nWayPoints
-                            x  = 1:nCachePoints;
-                            xq = linspace(1, nCachePoints, nWayPoints);
-                            zLine = interp1(x, zLine, xq);
+                        nCachePoints = height(wayPoints3D);
+                        
+                        if nWayPoints ~= nCachePoints
+                            z  = 1:nCachePoints;
+                            zq = linspace(1, nCachePoints, nWayPoints);
+                            wayPoints3D = [wayPoints2D, interp1(z, wayPoints3D(:,3), zq)'];
                         end
     
                     case 'MathWorks WMS Server'
                         load(fileName, 'zMatrix', 'zMatrixReference')
-                        zLine = ZLine(obj, wayPoints2D, zMatrix, zMatrixReference);
+                        wayPoints3D = WayPoints3D(obj, wayPoints2D, zMatrix, zMatrixReference);
                 end
             end
         end
 
         %-----------------------------------------------------------------%
-        function zLine = ZLine(obj, wayPoints2D, zMatrix, zMatrixReference)
-            zLine = [wayPoints2D, geointerp(zMatrix, zMatrixReference, wayPoints2D(:,1), wayPoints2D(:,2), 'nearest')];
+        function wayPoints2D = WayPoints2D(obj, txInfo, rxInfo, nPoints)
+            wayPoints2D = gcwaypts(txInfo.Latitude, txInfo.Longitude, rxInfo.Latitude, rxInfo.Longitude, nPoints-1);
+        end
+
+        %-----------------------------------------------------------------%
+        function wayPoints3D = WayPoints3D(obj, wayPoints2D, zMatrix, zMatrixReference)
+            wayPoints3D = [wayPoints2D, geointerp(zMatrix, zMatrixReference, wayPoints2D(:,1), wayPoints2D(:,2), 'nearest')];
         end
     end
 end
