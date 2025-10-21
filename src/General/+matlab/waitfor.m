b31d7a428d946a827458c4705a0f600de8c60172
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2025-10-21 16:56:25 -0400

    Update waitfor.m

--- src/General/+matlab/waitfor.m
+++ src/General/+matlab/waitfor.m
@@ -1,10 +1,27 @@
-function waitfor(obj, propName, condition, PAUSE, TIMEOUT)
+function waitfor(obj, propName, condition, PAUSE, TIMEOUT, ORIENTATION)
+
+    arguments
+        obj
+        propName
+        condition
+        PAUSE
+        TIMEOUT
+        ORIENTATION char {mustBeMember(ORIENTATION, {'propValue', 'propName'})} = 'propValue'
+    end
 
     tWaitFor = tic;
     while toc(tWaitFor) < TIMEOUT
-        if condition(obj.(propName))
-            break
+        switch ORIENTATION
+            case 'propValue'
+                if condition(obj.(propName))
+                    break
+                end
+            case 'propName'
+                if condition(propName)
+                    break
+                end
         end
+
         pause(PAUSE)
     end
 
